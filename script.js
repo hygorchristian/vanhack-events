23ed14fa4dfb8de5f7729ed5262caa4758384de2
Hygor <hygor.christian@gmail.com>
2020-07-21 20:55:05 -0300

    feat: adding alerts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const meta = {
+const metadata = {
   meetup: {
     name: 'MeetUp',
   },
@@ -9,7 +9,8 @@ const meta = {
   },
   recruiting: {
     name: 'Recruiting Mission',
-    class: 'highlight'
+    class: 'highlight',
+    icon: 'search',
   },
   vanhackathon: {
     name: 'VanHackathon',
@@ -23,63 +24,72 @@ const meta = {
     name: 'Premium Webinar',
     class: 'premium',
     icon: 'crown',
+    subscribe: 'premiumSubscribe()'
   }
 }
 
 function renderCardHeader(data){
-  const metadata = meta[data.type];
+  const meta = metadata[data.type];
 
   return `
-    <div class="badge ${metadata.class}">
+    <div class="badge ${meta.class}">
       <span class="badge-label">
-        ${metadata.name}
+        ${meta.name}
       </span>
     </div>
     <div class="info">
-      ${ metadata.icon ? `<div class="round ${metadata.class}"><i class="fas fa-${metadata.icon}"></i></div>` : '' }
+      ${ meta.icon ? `<div class="round ${meta.class}"><i class="fas fa-${meta.icon}"></i></div>` : '' }
     </div>
   `
 }
 
-function renderFront(data){
+function renderCardFront(item){
+  const meta = metadata[item.type];
+
   return `
-    <div class="flip-card-front ${meta[data.type].class}">
-      <div style="background-image: url('${data.image}')" class="card-header">
+    <div class="flip-card-front ${meta.class}">
+      <div style="background-image: url('${item.image}')" class="card-header">
         <div class="card-header-content">
-          ${renderCardHeader(data)}          
+          ${renderCardHeader(item)}          
         </div>
       </div>
       <div class="card-content">
         <div class="date">
-          <span class="month ${meta[data.type].class}">SEP</span>
+          <span class="month ${meta.class}">SEP</span>
           <span class="day">18</span>
         </div>
         <div class="info">
-          <span class="title">Dream World Wide in Jakarta</span>
-          <span class="description">Soehanna, Daerah Khusus Ibukota Yogyakarta, Indonesia</span>
+          <span class="title">${item.title}</span>
+          <span class="description">${item.description.substr(0, 100)}...</span>
         </div>
       </div>
     </div>
   `
 }
 
-function renderBack(data){
+function renderCardBack(item){
+  const meta = metadata[item.type];
+
   return `
-    <div class="flip-card-back ${meta[data.type].class}">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+    <div class="flip-card-back ${meta.class}">
+      <div class="info">
+        <span class="title">${item.title}</span>
+        <span class="description">${item.description}</span>
+      </div>
+      <button class="btn" onclick="${meta.subscribe}">
+        Checkout Now
+      </button>
     </div>
   `
 }
 
-function generateCard(data) {
+function generateCard(item) {
   const card = document.createElement('div');
   const html = `
     <div class="flip-card">
       <div class="flip-card-inner">
-        ${renderFront(data)}
-        ${renderBack(data)}        
+        ${renderCardFront(item)}
+        ${renderCardBack(item)}        
       </div>
     </div>
   `
@@ -99,12 +109,51 @@ async function loadItems() {
   })
 }
 
+/* Actions */
+
 function showSuccessMessage() {
   Swal.fire({
     icon: 'success',
     title: 'Your work has been saved',
     showConfirmButton: false,
     variant: 'error'
+  })
+}
+
+function premiumSubscribe() {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-premium ml-8',
+      cancelButton: 'btn'
+    },
+    buttonsStyling: false,
+    iconHtml: '<span>sdfsdfsdf</span>',
+  })
+
+  swalWithBootstrapButtons.fire({
+    title: 'You are not premium',
+    text: "Join our premium community ;)",
+    iconHtml: '<h1>sdfsdfsdf</h1>',
+    showCancelButton: true,
+    confirmButtonText: 'Become premium!',
+    cancelButtonText: 'No, cancel',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.value) {
+      swalWithBootstrapButtons.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'info'
+      )
+    } else if (
+        result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'info'
+      )
+    }
   })
 }
 
